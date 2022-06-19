adc386095db47953968acaf911a73f1073af9734
alfajri23 <123190088@student.upnyk.ac.id>
2022-06-19 10:20:18 +0700

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavbarFull from "./components/Navbar/NavbarFull";
-import { NavbarTitle } from "./components/Navbar/NavbarTitle";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Buyer from "./pages/Buyer";
-import HalamanProduk from "./pages/Buyer/HalamanProduk";
 import RouterPage from "./routes/RouterPage";
 
 function App() {
